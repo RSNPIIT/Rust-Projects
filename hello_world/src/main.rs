22d1eeb2d2a8c093f17cda304433007116006d19
RSNPIIT <ramrupsatpati@gmail.com>
2026-04-26 11:11:56 +0530

    Added further lines to the same

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, all\nNamaste from RustLang\nI am Ramrup Satpati");
+    println!("This is from Rust Cargo Project\nThis is my second attempt at Rust\n");
+    print!("This is print! similar to Python's print this will print in the same line unless i give a forward slash n only then");
 }
