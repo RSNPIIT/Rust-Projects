5aa657c1b6a856e1db40de67ebfe88ab45fb56ef
RSNPIIT <ramrupsatpati@gmail.com>
2026-05-16 05:50:21 +0530

    Added the File Here to study about Rust Variables

---
+++ hello_world/src/bin/varibl.rs
@@ -0,0 +1,8 @@
+fn main(){
+    let apples: i32 = 50;
+    let oranges: i32 = 40;
+    let marks: f64 = 98.45;
+    let name: &str = "Ramrup";
+    let fruits: i32 = apples + oranges;
+    println!("Hello I am {name}.\nI have {marks}% marks\nI have {apples} number of Apples\nI have {oranges} number of oranges\nI have total {fruits} number of Fruits");
+}
