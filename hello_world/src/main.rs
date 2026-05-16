7228df23c986b604d4a3fba920296e287e4429a0
RSNPIIT <ramrupsatpati@gmail.com>
2026-05-16 06:10:33 +0530

    Added the Starting File here

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
-    println!("Hello, all\nNamaste from RustLang\nI am Ramrup Satpati");
-    println!("This is from Rust Cargo Project\nThis is my second attempt at Rust\n");
-    print!("This is print! similar to Python's print this will print in the same line unless i give a forward slash n only then");
+    let sym = "-".repeat(50);
+    println!("{sym}");
+    println!("* Hello, all");
+    println!("* Namaste from RustLang");
+    println!("* I am Ramrup Satpati (aka RSNPIIT) from IITM");
+    println!("* Status: Python developer");
+    println!("{sym}");
+    println!("This cargo directory contains all my work done in Rust.");
+    println!("License: Copyleft GNU GPLv3");
+    println!("{sym}");
 }
